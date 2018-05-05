63005ad939214de919b1f4bef3769862fb04a24a
sanjay-gupta <sudhakar.mishra@actiknowbi.com>
2018-05-05 09:41:30 +0530

    Created Test Def Controller

---
+++ ATS.Service/Controllers/TestBankController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ATS.Core.Model;
+using ATS.Repository.DAO;
+using ATS.Repository.Interface;
+
+namespace ATS.Service.Controllers
+{
+    public class TestBankController : ApiController
+    {
+        private ITestRepository repository = null;
+        public TestBankController()
+        {
+            repository = new TestBankRepository();
+        }
+
+        [HttpPost]
+        [Route("api/TestBank/Create")]
+        public IHttpActionResult Create(TestBank testBank)
+        {
+            var result = repository.Create(testBank);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("api/TestBank/Update")]
+        public IHttpActionResult Update(TestBank testBank)
+        {
+            var result = repository.Update(testBank);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("api/TestBank/Delete")]
+        public IHttpActionResult Delete(TestBank testBank)
+        {
+            var result = repository.Delete(testBank);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("api/TestBank/Retrieve")]
+        public IHttpActionResult Retrieve(TestBank testBank)
+        {
+            var result = repository.Retrieve(testBank);
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        [Route("api/TestBank/Select")]
+        public IHttpActionResult Select(params object[] inputs)
+        {
+            var result = repository.Select(inputs);
+            return Ok(result);
+        }
+    }
+}
--- ATS.Service/Controllers/TypeDefController.cs
+++ ATS.Service/Controllers/TypeDefController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ATS.Core.Model;
 using ATS.Repository.DAO;
 using ATS.Repository.Interface;
 
@@ -11,11 +12,51 @@ namespace ATS.Service.Controllers
 {
     public class TypeDefController : ApiController
     {
-        private ITypeRepository typeRepository = null;
+        private ITypeRepository repository = null;
         public TypeDefController()
         {
-            typeRepository = new TypeDefRepository();
-        }  
-        
+            repository = new TypeDefRepository();
+        }        
+
+        [HttpPost]
+        [Route("api/TypeDef/Create")]
+        public IHttpActionResult Create(TypeDef typeDef)
+        {
+            var result = repository.Create(typeDef);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("api/TypeDef/Update")]
+        public IHttpActionResult Update(TypeDef typeDef)
+        {
+            var result = repository.Update(typeDef);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("api/TypeDef/Delete")]
+        public IHttpActionResult Delete(TypeDef typeDef)
+        {
+            var result = repository.Delete(typeDef);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("api/TypeDef/Retrieve")]
+        public IHttpActionResult Retrieve(TypeDef typeDef)
+        {
+            var result = repository.Retrieve(typeDef);
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        [Route("api/TypeDef/Select")]
+        public IHttpActionResult Select(params object[] inputs)
+        {
+            var result = repository.Select(inputs);
+            return Ok(result);
+        }
     }
 }
