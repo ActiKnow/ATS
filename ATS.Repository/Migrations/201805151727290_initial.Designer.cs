2552b9b16746e9a087056679defc7e89f9ba3a75
deepika-gupta91 <deepika.gupta@actiknow.com>
2018-05-16 13:59:13 +0530

    Migration File

--- ATS.Repository/Migrations/201805151727290_initial.Designer.cs
+++ ATS.Repository/Migrations/201805151727290_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace ATS.Repository.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class InitialMigration : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201805160609565_InitialMigration"; }
+            get { return "201805151727290_initial"; }
         }
         
         string IMigrationMetadata.Source
