2b4b405dc9233047be5ea92ccec2805632b5cabd
Shobhit-Rawat <avnish.kumar@actiknowbi.com>
2018-05-17 12:09:06 +0530

    Question subjective : Creation Bug fixed

---
+++ ATS.Web/Scripts/module/test-setup/map-test-question.js
@@ -0,0 +1,43 @@
+﻿var mapTestQuestion = (function () {
+    'use strict';
+    var defaults = {};
+    var action = {
+
+    };
+    var render = {
+        openSelectTest: function () {
+            var op = defaults;
+            var $testModal = $(op.modalSelectTestContext);
+            $testModal.modal('show');
+        },
+        opernSelectQues: function () {
+            var op = defaults;
+            var $quesModal = $(op.modalSelectQuesContext);
+            $quesModal.modal('show');
+        },
+    };
+    var loader = {
+        loadEvents: function () {
+            var op = defaults;
+            var $testContext = $(op.testContext);
+            var $quesContext = $(op.quesContext);
+
+            $testContext.on('click', op.openSelectTest, render.openSelectTest);
+            $quesContext.on('click', op.openSelectQues, render.opernSelectQues);
+        },
+    };
+    var setup = function () {
+        for (let index in loader) {
+            if (typeof loader[index] == 'function') {
+                loader[index]();
+            }
+        }
+    };
+    var init = function (config) {
+        $.extend(true, defaults, config);
+        setup();
+    };
+    return {
+        init: init
+    };
+})();
